69379315c3898fe8b884782b97022f431598103e
Usuario <Usuario@DESKTOP-KJS75U2>
2020-03-25 11:05:34 -0600

    otherCommit

---
+++ PROYECT/src/proyectoooo/ClienteP.java
@@ -0,0 +1,5 @@
+package proyectoooo;
+
+public class ClienteP {
+
+}
---
+++ PROYECT/src/proyectoooo/NodoP.java
@@ -0,0 +1,13 @@
+package proyectoooo;
+
+public class NodoP {
+	ClienteP info;
+	NodoP siguiente;
+	
+	public NodoP( ClienteP info, NodoP siguiente) {
+		this.info= info;
+		this.siguiente= siguiente;
+	}
+
+
+}
