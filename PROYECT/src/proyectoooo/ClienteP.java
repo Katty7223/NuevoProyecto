9bb0fdae4cbee762bf5cfc0f20126fd13dcf2e79
Usuario <Usuario@DESKTOP-KJS75U2>
2020-03-25 11:11:51 -0600

    agregando la clase cliente

--- PROYECT/src/proyectoooo/ClienteP.java
+++ PROYECT/src/proyectoooo/ClienteP.java
@@ -2,4 +2,27 @@ package proyectoooo;
 
 public class ClienteP {
 
+	
+	String nombre="";
+	String nit="";
+	int fechaN;
+	String tel="";
+	int  horaEnt;
+	String placa="";
+	String dpi="";
+	String modelo="";
+	
+	
+	public ClienteP(String nombre, String nit, int fechaN, String tel,  int horaEnt, String placa, String dpi, String modelo) {
+		this.nombre=nombre;
+		this.nit=nit;
+		this.fechaN=fechaN;
+		this.tel=tel;
+		this.horaEnt=horaEnt;
+		this.placa=placa;
+		this.dpi=dpi;
+		this.modelo=modelo;
+		
+	}
+
 }
